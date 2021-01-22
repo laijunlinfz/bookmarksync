3cfcaa977a737de873b0232ffa7a0b495ffb7fcf
laijl <laijlfz@gmail.com>
2021-01-22 19:06:51 +0800

    style: test4

--- src/popup/pages/login/index.tsx
+++ src/popup/pages/login/index.tsx
@@ -14,7 +14,7 @@ import { ChromeEventType } from "@/types/background";
 let TIME = 120;
 let INTERVAL: NodeJS.Timeout;
 
-// test3
+// test4
 const Login: React.FC = () => {
   const history: History = useHistory();
   const [email, setEmail] = useState<string>("3@qq.com");
